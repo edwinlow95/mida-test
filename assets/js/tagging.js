33065ed5a5d96b4bbaf482b4c12a483a5e91c147
edwinlow95 <edwinlow@convertium.com>
2026-02-25 16:13:24 +0800

    commented out

--- assets/js/tagging.js
+++ assets/js/tagging.js
@@ -7,41 +7,41 @@
 // AA Tracking
 const triggerAA = (eventName, eventValue) => {
   var message;
-  var messageObject;
+  // var messageObject;
 
   // get the global object data
   const mdq = window.mdq || [];
 
-  // set the events
-  mdq.events = mdq.events || [];
-  mdq.events.eventName = eventName;
-  mdq.events.eventPayload = eventValue;
+  // // set the events
+  // mdq.events = mdq.events || [];
+  // mdq.events.eventName = eventName;
+  // mdq.events.eventPayload = eventValue;
 
-  message = `AA Debug:
-  event_name: ${eventName},
-  event_value: ${eventValue}`;
-  messageObject = { eventName, eventValue };
+  // message = `AA Debug:
+  // event_name: ${eventName},
+  // event_value: ${eventValue}`;
+  // messageObject = { eventName, eventValue };
 
-  if (typeof _satellite !== 'undefined') {
-    if (typeof window.Granite !== 'undefined' && typeof window.Granite.author !== 'undefined') {
-      console.info('Trigger satellite track');
-    }
-    _satellite.track(mdq.events.eventName);
-  }
+  // if (typeof _satellite !== 'undefined') {
+  //   if (typeof window.Granite !== 'undefined' && typeof window.Granite.author !== 'undefined') {
+  //     console.info('Trigger satellite track');
+  //   }
+  //   _satellite.track(mdq.events.eventName);
+  // }
 
-  if (typeof window.Granite !== 'undefined' && typeof window.Granite.author !== 'undefined') {
-    console.debug(mdq);
+  // if (typeof window.Granite !== 'undefined' && typeof window.Granite.author !== 'undefined') {
+  //   console.debug(mdq);
 
-    // Debug - uncomment during development
-    if (window.console) {
-      if (typeof eventValue === 'object') {
-        console.log('AA Debug: ');
-        console.log(JSON.stringify(messageObject));
-      } else {
-        console.log(message);
-      }
-    }
-  }
+  //   // Debug - uncomment during development
+  //   if (window.console) {
+  //     if (typeof eventValue === 'object') {
+  //       console.log('AA Debug: ');
+  //       console.log(JSON.stringify(messageObject));
+  //     } else {
+  //       console.log(message);
+  //     }
+  //   }
+  // }
 
   // For testing purpose only
   if (window.console) {
